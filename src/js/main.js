3ece1fe89cc3363e402602f68b63ddd74b5ab219
ilich <st.rowdy@gmail.com>
2020-04-03 02:53:29 +0300

    +header

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
 $(function(){
 
     $('.header__slider').slick({
-        infinite:true
+        infinite:true,
+        fade:true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="../../build/img/arrows-left.svg" alt="Назад">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="../../build/img/arrows-right.svg" alt="Далее">',
+        asNavFor: '.slider-dots'
+
+    });
+
+    $('.slider-dots').slick({
+        infinite: true,
+        slidesToShow: 4,
+        asNavFor: '.header__slider'
     });
 
 });
