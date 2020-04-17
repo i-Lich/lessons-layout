702ec7d6577eb1995696731fc4bcaef99a5612f0
ilich <st.rowdy@gmail.com>
2020-04-17 23:25:59 +0300

    +3 секция travel

--- gosurf/src/js/main.js
+++ gosurf/src/js/main.js
@@ -35,4 +35,12 @@ $(function(){
         asNavFor: '.surf-slider',
         focusOnSelect: true
     });
+    $('.travel__slider').slick({
+        infinite:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade:true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="../../build/img/arrows-left.svg" alt="Назад">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="../../build/img/arrows-right.svg" alt="Далее">',
+    });
 });
