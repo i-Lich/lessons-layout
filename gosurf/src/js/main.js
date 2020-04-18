330aa5ded27a741afee45acf4158d99a04bfb49a
ilich <st.rowdy@gmail.com>
2020-04-18 03:02:46 +0300

    +4 секция SLEEP, начало

--- gosurf/src/js/main.js
+++ gosurf/src/js/main.js
@@ -35,7 +35,7 @@ $(function(){
         asNavFor: '.surf-slider',
         focusOnSelect: true
     });
-    $('.travel__slider').slick({
+    $('.holder__slider').slick({
         infinite:true,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -43,4 +43,38 @@ $(function(){
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="../../build/img/arrows-left.svg" alt="Назад">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="../../build/img/arrows-right.svg" alt="Далее">',
     });
+
+    $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="../../build/img/plus.svg" alt="plus"></div><div class="quantity-button quantity-down"><img src="../../build/img/minus.svg" alt="minus"></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+        var spinner = $(this),
+            input = spinner.find('input[type="number"]'),
+            btnUp = spinner.find('.quantity-up'),
+            btnDown = spinner.find('.quantity-down'),
+            min = input.attr('min'),
+            max = input.attr('max');
+
+        btnUp.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue >= max) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue + 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+        btnDown.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue <= min) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue - 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+    });
+
 });
